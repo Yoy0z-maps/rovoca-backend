f2e05c39c2f1f1bad9a1e192e3a7002d0e992474
John Han <work.johnhan@gmail.com>
2025-06-11 20:48:36 +0800

    Fix: word: urls.py DefaultRouter 수정

--- word/urls.py
+++ word/urls.py
@@ -1,31 +1,10 @@
 from django.urls import path
 from .views import WordView, WordbookView
 
-wordbook_list = WordbookView.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-wordbook_detail = WordbookView.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+from rest_framework.routers import DefaultRouter
 
-word_list = WordView.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-word_detail = WordView.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+router = DefaultRouter()
+router.register('wordbooks', WordbookView, basename='wordbook')
+router.register('words', WordView, basename='word')
 
-urlpatterns = [
-    path('wordbooks/', wordbook_list, name='wordbook-list'),
-    path('wordbooks/<int:pk>/', wordbook_detail, name='wordbook-detail'),
-    path('words/', word_list, name='word-list'),
-    path('words/<int:pk>/', word_detail, name='word-detail'),
-]
+urlpatterns = router.urls
