2dbdda1fde7ce8126d66ba40e81c4c18a0acac16
John Han <work.johnhan@gmail.com>
2025-10-06 23:00:50 +0900

    TEST

--- users/scheduler.py
+++ users/scheduler.py
@@ -9,10 +9,10 @@ def start():
     scheduler = BackgroundScheduler(timezone=timezone("Asia/Seoul"))
 
     # 오전 8시 가장 오래된 / 최근 단어 알림
-    scheduler.add_job(send_push_with_word, CronTrigger(hour=22, minute=59))
+    scheduler.add_job(send_push_with_word, CronTrigger(hour=23, minute=1))
 
     # 오후 8시 전체 유저 복습 알림
-    scheduler.add_job(send_push_to_all_users, CronTrigger(hour=20, minute=-0))
+    scheduler.add_job(send_push_to_all_users, CronTrigger(hour=20, minute=0))
 
     # 매일 밤 10시 비활성 유저 알림
     scheduler.add_job(send_push_to_inactive_users, CronTrigger(hour=22, minute=0))
