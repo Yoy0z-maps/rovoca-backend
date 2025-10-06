6704b31298308dee21855338f555f9f6ec1843d1
John Han <work.johnhan@gmail.com>
2025-10-07 00:11:43 +0900

    TEST

--- users/apps.py
+++ users/apps.py
@@ -1,16 +1,7 @@
 # users/apps.py
 
 from django.apps import AppConfig
-import os
-import threading
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        # 프로세스당 한 번만 실행되도록 보장
-        if not hasattr(self, '_scheduler_started'):
-            self._scheduler_started = True
-            from .scheduler import start
-            start()
