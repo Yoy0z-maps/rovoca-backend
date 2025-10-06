a749fe8156e893a0274536db30c9850a1d681d42
John Han <work.johnhan@gmail.com>
2025-10-06 23:08:52 +0900

    TEST

--- users/apps.py
+++ users/apps.py
@@ -2,15 +2,15 @@
 
 from django.apps import AppConfig
 import os
+import threading
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
-        # 개발 환경에서 중복 실행 방지
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
-            
-        from .scheduler import start
-        start()
+        # 프로세스당 한 번만 실행되도록 보장
+        if not hasattr(self, '_scheduler_started'):
+            self._scheduler_started = True
+            from .scheduler import start
+            start()
--- users/scheduler.py
+++ users/scheduler.py
@@ -18,7 +18,7 @@ def start():
     scheduler = BackgroundScheduler(timezone=timezone("Asia/Seoul"))
 
     # 오전 8시 가장 오래된 / 최근 단어 알림
-    scheduler.add_job(send_push_with_word, CronTrigger(hour=23, minute=8))
+    scheduler.add_job(send_push_with_word, CronTrigger(hour=23, minute=10))
 
     # 오후 8시 전체 유저 복습 알림
     scheduler.add_job(send_push_to_all_users, CronTrigger(hour=20, minute=0))
